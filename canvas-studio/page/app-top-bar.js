90c74973edc4ae3d7bc85587119754f8e2655b7b
Johnny Wu <KarasAya@gmail.com>
2015-06-30 23:17:44 +0800

    remove unused button from topbar

--- canvas-studio/page/app-top-bar.js
+++ canvas-studio/page/app-top-bar.js
@@ -4,12 +4,4 @@ Polymer({
     _onProject: function () {
         Editor.sendToCore('app:explore-project');
     },
-
-    _onAssets: function () {
-        Editor.sendToCore('app:explore-assets');
-    },
-
-    _onLibrary: function () {
-        Editor.sendToCore('app:explore-library');
-    },
 });
