32c20fabdfd048c6ba0cf386aacc771670d28f65
Nan Wang <nantas@gmail.com>
2015-07-20 10:20:09 +0800

    #70 add preview server back in canvas-studio/core, start server in init.js. need to install express.

--- canvas-studio/core/init.js
+++ canvas-studio/core/init.js
@@ -43,6 +43,7 @@ require('./scene-utils');
 // asset-db ipc
 require('./asset-db-ipc');
 
+startPreviewServer();
 
 // ====================
 // register events
@@ -55,3 +56,12 @@ Editor.events.on('focus', function () {
 Editor.events.on('blur', function () {
     Editor.assetdb.watchON();
 });
+
+// =======================
+// preview server
+// =======================
+
+function startPreviewServer() {
+    var server = require('./preview-server');
+    server.start();
+}
