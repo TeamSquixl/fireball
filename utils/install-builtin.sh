67ff2c7cc5549c44c1b9f6a026ea9bfbf215f51b
Johnny Wu <KarasAya@gmail.com>
2015-08-07 00:28:15 +0800

    update builtin

--- utils/install-builtin.sh
+++ utils/install-builtin.sh
@@ -6,95 +6,35 @@ if [ ! -d "builtin" ]; then
 fi
 cd builtin
 
-# builtin dev helper
+repos=( \
+asset-db-debugger \
+assets \
+builder \
+code-editor \
+color-picker \
+console \
+fire-assets \
+fire-gizmos \
+fire-ui-kit \
+hierarchy \
+inspector \
+ipc-debugger \
+package-manager \
+release-helper \
+scene \
+tester \
+timeline \
+ui-grid \
+ui-kit \
+ui-property \
+ui-tree \
+)
 
-if [ ! -d "asset-db-debugger" ]; then
-    git clone https://github.com/fireball-packages/asset-db-debugger
-fi
-
-if [ ! -d "console" ]; then
-    git clone https://github.com/fireball-packages/console
-fi
-
-if [ ! -d "ipc-debugger" ]; then
-    git clone https://github.com/fireball-packages/ipc-debugger
-fi
-
-if [ ! -d "package-manager" ]; then
-    git clone https://github.com/fireball-packages/package-manager
-fi
-
-if [ ! -d "release-helper" ]; then
-    git clone https://github.com/fireball-packages/release-helper
-fi
-
-if [ ! -d "tester" ]; then
-    git clone https://github.com/fireball-packages/tester
-fi
-
-# builtin widgets
-if [ ! -d "ui-grid" ]; then
-    git clone https://github.com/fireball-packages/ui-grid
-fi
-
-if [ ! -d "ui-kit" ]; then
-    git clone https://github.com/fireball-packages/ui-kit
-fi
-
-if [ ! -d "ui-property" ]; then
-    git clone https://github.com/fireball-packages/ui-property
-fi
-
-if [ ! -d "ui-tree" ]; then
-    git clone https://github.com/fireball-packages/ui-tree
-fi
-
-if [ ! -d "color-picker" ]; then
-    git clone https://github.com/fireball-packages/color-picker
-fi
-
-# builtin panels
-if [ ! -d "assets" ]; then
-    git clone https://github.com/fireball-packages/assets
-fi
-
-if [ ! -d "hierarchy" ]; then
-    git clone https://github.com/fireball-packages/hierarchy
-fi
-
-if [ ! -d "inspector" ]; then
-    git clone https://github.com/fireball-packages/inspector
-fi
-
-if [ ! -d "scene" ]; then
-    git clone https://github.com/fireball-packages/scene
-fi
-
-if [ ! -d "timeline" ]; then
-    git clone https://github.com/fireball-packages/timeline
-fi
-
-if [ ! -d "builder" ]; then
-    git clone https://github.com/fireball-packages/builder
-fi
-
-if [ ! -d "code-editor" ]; then
-    git clone https://github.com/fireball-packages/code-editor
-fi
-
-# builtin fireball packages
-
-if [ ! -d "fire-ui-kit" ]; then
-    git clone https://github.com/fireball-packages/fire-ui-kit
-fi
-
-if [ ! -d "fire-assets" ]; then
-    git clone https://github.com/fireball-packages/fire-assets
-fi
-
-if [ ! -d "fire-gizmos" ]; then
-    git clone https://github.com/fireball-packages/fire-gizmos
-fi
-
+for name in "${repos[@]}"
+do
+    if [ ! -d "${name}" ]; then
+        git clone https://github.com/fireball-packages/${name}
+    fi
+done
 
 cd ${ORIGINAL_PATH}
