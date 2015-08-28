2158f2b44499b3ef5fef60a3d37a471a6ed90bf5
Nan Wang <nantas@gmail.com>
2015-08-28 15:41:52 +0800

    add a hack to fix jade flatten missing package issue.

--- gulpfile.js
+++ gulpfile.js
@@ -655,10 +655,30 @@ gulp.task('flatten-modules', function(cb) {
     var flatten = require('flatten-packages');
     flatten(appLoc, {}, function (err, res) {
       if (err) console.error(err);
-      if (res) return console.log(res);
+      if (res) {
+          console.log(res);
+          return cb();
+      }
     });
 });
 
-gulp.task('make-dist-mac', gulpSequence('rename-electron-mac', 'copy-app-dist', 'flatten-modules'));
+gulp.task('make-dist-mac', gulpSequence('rename-electron-mac', 'copy-app-dist', 'flatten-modules', 'copy-missing-packages'));
 
-gulp.task('make-dist-win', gulpSequence('rename-electron-win', 'copy-app-dist', 'flatten-modules'));
+gulp.task('make-dist-win', gulpSequence('rename-electron-win', 'copy-app-dist', 'flatten-modules', 'copy-missing-packages'));
+
+// HACK dist version jade module
+
+gulp.task('copy-missing-packages', function(cb) {
+    if (process.platform === 'win32') {
+        Fs.copySync('node_modules/xmldom', 'dist/Fireball.app/Contents/Resources/app/node_modules/xmldom');
+    }
+    Fs.copySync('node_modules/jade', 'dist/Fireball.app/Contents/Resources/app/node_modules/jade', {clobber: true});
+    var flatten = require('flatten-packages');
+    flatten('dist/Fireball.app/Contents/Resources/app/node_modules/jade', {}, function (err, res) {
+      if (err) console.error(err);
+      if (res) {
+          console.log(res);
+          return cb();
+      }
+    });
+});
